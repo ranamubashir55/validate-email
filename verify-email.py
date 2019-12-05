56c0324acee50f4cd04a1db1b2792db98a3ea390
alishahaan9 <alishahaan9@gmail.com>
2019-12-05 16:43:53 +0500

    latest

--- verify-email.py
+++ verify-email.py
@@ -293,4 +293,4 @@ def get_email():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, threaded=True, debug=True)
+    app.run(host="0.0.0.0", port=80, threaded=True, debug=True)
